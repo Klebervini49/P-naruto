843d6c448ef7c8166a4113a642b6ec4dea08697f
Kleberson Vinicius <klebervini49@gmail.com>
2022-01-10 10:10:06 -0300

    Recriando o codigo

---
+++ personagens.js
@@ -0,0 +1,70 @@
+let personagens = [{
+    id: 1,
+    nome: 'naruto uzumaki',
+    desc: '<b>Naruto Uzumaki</b> é apresentado como um jovem rapaz loiro, cabelos arrepiados e olhos azuis. Ele frequentemente veste uma jaqueta laranja. Após a sua graduação ninja ele começa a usar um protetor de testa que denota a sua posição como um ninja da Vila Oculta da Folha',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/naruto.png',
+    alt: 'Imagem personagem Naruto Uzumaki',
+},
+{
+    id: 2,
+    nome: 'sasuke uchiha',
+    desc: '<b>Sasuke Uchiha</b> é um ninja de pele clara que tem olhos ônix e cabelos preto na altura do queixo. Seu cabelo é espetado nas costas com franjas que crescem com o avançar da história. Quando criança, sua franja ficava pendurada acima de seus olhos.',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/sasuke.png',
+    alt: 'Imagem personagem Sasuke Uchiha',
+
+},
+{
+    id: 3,
+    nome: 'sakura haruno',
+    desc: '<b>Sakura Haruno</b> é uma garota inteligente, forte e decidida. A sua principal característica é o seu amor em relação à Sasuke Uchiha desde o início da série, e mesmo esse tendo se tornado uma criminoso, ela ainda mostra ter afeto por ele.',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/sakura.png',
+    alt: 'Imagem personagem Sakura Haruno',
+
+},
+{
+    id: 4,
+    nome: 'Jiraiya',
+    desc: '<b>Jiraiya</b> foi um dos Sannin Lendários, junto com Tsunade e Orochimaru, treinados por Hiruzen Sarutobi. Ele era um auto-proclamado super-pervertido, e o escritor da popular série de livros de ficção para adultos, chamado Icha Icha. Ele também era conhecido como o Sábio dos Sapos, por causa da sua assinatura de invocação de sapos, e por seu status como sábio.',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/jiraiya.png',
+    alt: 'Imagem personagem Jiraiya',
+
+},
+{
+    id: 5,
+    nome: 'Kakashi Hatake',
+    desc: '<b>Kakashi Hatake</b> é um shinobi de Konohagakure. Ele recebeu um Sharingan de seu ex-companheiro de equipe, Obito Uchiha, quando era mais jovem, fazendo-o ser conhecido como Kakashi o Ninja Copiador.',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/kakashi.png',
+    alt: 'Imagem personagem Kakashi Hatake',
+},
+{
+    id: 6,
+    nome: 'Itachi Uchiha',
+    desc: '<b>Itachi Uchiha</b> foi um prodígio do clã Uchiha de Konohagakure. Ele se tornou um criminoso internacional depois de assassinar seu clã inteiro, poupando apenas seu irmão mais novo, Sasuke. Ele se juntou a Akatsuki, onde ele entrou em frequentes conflitos com Konoha e seus ninjas, incluindo Sasuke, que procurou vingar sua família. Depois de morrer durante uma batalha com Sasuke, as motivações de Itachi foram reveladas serem mais complicadas do que pareciam ele só queria proteger seu irmão e a vila, permanecendo um shinobi leal a Konohagakure até o fim.',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/itachi.png',
+    alt: 'Imagem personagem Itachi Uchiha',
+},
+{
+    id: 7,
+    nome: 'hinata Hyūga',
+    desc: '<b>Hinata Uzumaki</b> é a maior personagem de assistência da série. Ela é um membro dos clãs Hyūga e Uzumaki de Konohagakure. Tendo um temperamento humilde, a falta de auto-confiança de Hinata era refletida em suas habilidades — traços que fizeram seu pai considerar que ela não era adequada para a sua posição como herdeira do clã. A partir de suas observações com relação a Naruto Uzumaki, Hinata encontra tanto um exemplo a seguir para ser mais assertiva, como uma pessoa para amar. Em sua filiação com o Time Kurenai, ela procura mudar e se tornar forte, mesmo sendo um pouco de cada vez.',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/hinata.png',
+    alt: 'Imagem personagem Hinata Hyūga',
+
+},
+{
+    id: 8,
+    nome: 'Orochimaru',
+    desc: '<b>Orochimaru</b> é um dos Lendários Sannin, junto com Tsunade e Jiraiya, de Konohagakure. Com sua ambição de vida sendo aprender todos os segredos do mundo, Orochimaru buscou a imortalidade para que ele pudesse viver todas as vidas necessárias para realizar sua tarefa. Depois de ser pego em flagrante realizando experimentos antiéticos sobre seus concidadãos por causa desta imortalidade, Orochimaru desertou de Konoha, passando a ser perseguido por suas ambições, e por muitos anos procurou a destruição da aldeia, a fim de se vingar e demonstrar o que ele tinha aprendido.',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/orochimaru.png',
+    alt: 'Imagem personagem Orochimaru',
+
+},
+{
+    id: 9,
+    nome: 'Nagato - Pain',
+    desc: '<b>Nagato</b> foi um órfão de Amegakure e um aluno de Jiraiya, que cresceu durante a Segunda Guerra Mundial Shinobi. Órfão pelo conflito, Nagato se uniu juntamente com seus companheiros órfãos de guerra para formar a Akatsuki, uma organização cujos objetivos foram parar os ciclos infinitos de morte. Depois de seu amigo e líder, Yahiko, ser morto por pregar a paz, Nagato se convenceu de que o mundo nunca iria parar de lutar voluntariamente a menos que ele conheça o que é a verdadeira dor. Ele adotou o pseudônimo de Pain (ペイン, Pein) e foi o novo líder da Akatsuki na tentativa de mudar o mundo a um estado de paz através do medo da destruição pelas mãos da Akatsuki.',
+    img: 'https://klebervini49.github.io/p-naruto/imagens/nagato.png',
+    alt: 'Imagem personagem Nagato - Pain',
+
+},
+]
